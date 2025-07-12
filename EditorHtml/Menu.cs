3a3d57a5e58ee28542bcd20ef4258ae0c62ec442
Ricardo Bueno <ricardo.sb.contato@gmail.com>
2025-07-12 15:57:05 -0300

    implementando menu com switch

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -13,6 +13,7 @@ namespace EditorHtml
             WriteOptions();
 
             var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
         }
 
         public static void DrawHorizontalLine()
@@ -59,6 +60,22 @@ namespace EditorHtml
             Console.SetCursorPosition(3, 10);
             Console.WriteLine("Opção: ");
             Console.SetCursorPosition(10, 10);
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Console.WriteLine("Editor"); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0:
+                    {
+                        Console.Clear();
+                        Environment.Exit(0);
+                        break;
+                    }
+                default: Show(); break;
+            }
         }
     }
 }
