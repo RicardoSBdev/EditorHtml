70f51b84009fa07ae6142266af8b3f988ef25564
Ricardo Bueno <ricardo.sb.contato@gmail.com>
2025-07-14 18:34:49 -0300

    implmentando negrito

--- EditorHtml/obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
+++ EditorHtml/obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1129412e1948bd82a3a1ef4bb632fb653507a01e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a3d57a5e58ee28542bcd20ef4258ae0c62ec442")]
 [assembly: System.Reflection.AssemblyProductAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
